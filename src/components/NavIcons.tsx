c770de8fbf1bbafa5fdec2d2c8de0804d97259c5
Stuart McFarlane <stuart@mcfarlane.nl>
2024-08-21 11:35:41 +0200

    hide navicons on click profile

--- src/components/NavIcons.tsx
+++ src/components/NavIcons.tsx
@@ -22,6 +22,10 @@ const NavIcons = () => {
         }
         setIsProfileOpen(prev => !prev)
     }
+    const handleProfileLink = () => {
+        setIsProfileOpen(prev => !prev)
+        router.push("/profile")
+    }
     const handleLogout = () => {
         logout()
         setIsProfileOpen(prev => !prev)
@@ -33,7 +37,10 @@ const NavIcons = () => {
             <Image src="/profile.png" alt="" width={22} height={22} className="cursor-pointer" onClick={handleProfile} />
             {isProfileOpen && (
                 <div className="absolute p-4 rounded-md top-12 right-0 bg-white text-sm shadow-[0_3px_10px_rgb(0,0,0,0.2)] z-10">
-                    <Link href="/profile">Profile</Link>
+                    <div
+                        className="mt-2 cursor-pointer"
+                        onClick={handleProfileLink}
+                    >Profile</div>
                     <div
                         className="mt-2 cursor-pointer"
                         onClick={handleLogout}
