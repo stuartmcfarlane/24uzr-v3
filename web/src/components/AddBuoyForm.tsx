4187b9d4a91ed1ce64c50b10b7052ea795c6a508
Stuart McFarlane <stuart@mcfarlane.nl>
2024-09-23 15:16:09 +0200

    whitespace

--- web/src/components/AddBuoyForm.tsx
+++ web/src/components/AddBuoyForm.tsx
@@ -8,10 +8,28 @@ const AddBuoyForm = (props: AddBuoyFormProps) => {
     const { map } = props
     return (
         <form action={createBuoy} className="flex flex-col gap-4 mt-4 border-t-2 pt-4">
-            <input type="text" name="name" placeholder="name" className="ring-2 ring-gray-300 rounded-md p-4" />
-            <input type="text" name="lat" placeholder="latitude" className="ring-2 ring-gray-300 rounded-md p-4"/>
-            <input type="text" name="lng" placeholder="longitude" className="ring-2 ring-gray-300 rounded-md p-4" />
-            <button className="bg-24uzr text-white p-2 rounded-md disabled:bg-24uzr-disabled disabled:cursor-not-allowed">Add buoy</button>
+            <input
+                type="text"
+                name="name"
+                placeholder="name"
+                className="ring-2 ring-gray-300 rounded-md p-4"
+            />
+            <input
+                type="text"
+                name="lat"
+                placeholder="latitude"
+                className="ring-2 ring-gray-300 rounded-md p-4"
+            />
+            <input
+                type="text"
+                name="lng"
+                placeholder="longitude"
+                className="ring-2 ring-gray-300 rounded-md p-4"
+            />
+            <button
+                type="submit"
+                className="bg-24uzr text-white p-2 rounded-md disabled:bg-24uzr-disabled disabled:cursor-not-allowed"
+            >Add buoy</button>
 
             <input type="hidden" name="mapId" value={map.id}/>
         </form>
