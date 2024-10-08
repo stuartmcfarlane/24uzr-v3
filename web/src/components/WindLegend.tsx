91d5d4bc92343312df53a522a14ca6dc8cd96d2d
Stuart McFarlane <stuart@mcfarlane.nl>
2024-10-08 12:34:22 +0200

    wind legend

--- web/src/components/WindLegend.tsx
+++ web/src/components/WindLegend.tsx
@@ -16,11 +16,32 @@ const WindLegend = (props: WindLegendProps) => {
     }
     return <div className="flex justify-between">
         <div>
-            <input type="checkbox" checked={showWind} onClick={onToggleWind} />
-            <label>Show wind</label>
+            <input
+                id="toggleWind"
+                type="checkbox"
+                checked={showWind}
+                onClick={onToggleWind}
+                className="ml-1 align-middle"
+            />
+            <label
+                htmlFor="toggleWind"
+                className="ml-1">
+                wind
+            </label>
         </div>
         <div>scroll</div>
-        <div>legend</div>
+        <div className="flex">
+            <div className={`bg-beufort-1 w-4`}></div>
+            <div className={`bg-beufort-2 w-4`}></div>
+            <div className={`bg-beufort-3 w-4`}></div>
+            <div className={`bg-beufort-4 w-4`}></div>
+            <div className={`bg-beufort-5 w-4`}></div>
+            <div className={`bg-beufort-6 w-4`}></div>
+            <div className={`bg-beufort-7 w-4`}></div>
+            <div className={`bg-beufort-8 w-4`}></div>
+            <div className={`bg-beufort-9 w-4`}></div>
+            <div className={`bg-beufort-10 w-4`}></div>
+        </div>
     </div>
 }
 
--- web/tailwind.config.ts
+++ web/tailwind.config.ts
@@ -17,6 +17,17 @@ const config: Config = {
         "24uzr": "#0ea5e9",
         "24uzr-disabled": "#bae6fd",
         "24uzr-red": "red",
+        "beufort-1": "rgb(134, 163, 171)",
+        "beufort-2": "rgb(15,147,167,256)",
+        "beufort-3": "rgb(57,163,57,256)",
+        "beufort-4": "rgb(200,66,13,256)",
+        "beufort-5": "rgb(175,80,136)",
+        "beufort-6": "rgb(117,74,147)",
+        "beufort-7": "rgb(194,251,119,256)",
+        "beufort-8": "rgb(241,255,109,256)",
+        "beufort-9": "rgb(256,256,256,256)",
+        "beufort-10": "rgb(0,256,256,256)"
+
       },
     },
   },
